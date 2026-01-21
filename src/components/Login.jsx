db4169d6d9dc9d1e12f9d49af6a534f48eddc060
ATAGWE ROGER <atagweroger2@gmail.com>
2026-01-21 12:11:04 +0100

    dev commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "../styles/SignUp.module.css";
+import styles from "../styles/Login.module.css";
 import { Link } from "react-router-dom";
 export default function Login() {
   const [email, setEmail] = useState("");
