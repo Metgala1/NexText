45c1fc4d7c953134c4bb230a6722f881a1bf09b7
ATAGWE ROGER <atagweroger2@gmail.com>
2026-01-21 12:15:03 +0100

    dev commit

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import  { useState } from "react";
-import styles from "../styles/SignUp.module.css";
+import styles from "../styles/Signup.module.css";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
