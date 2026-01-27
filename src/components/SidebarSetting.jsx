2e07f65d4fe8f669da80e35de80ac6170f918d5d
ATAGWE ROGER <atagweroger2@gmail.com>
2026-01-27 08:32:28 +0100

    Styled my page for mobile responsiveness

--- src/components/SidebarSetting.jsx
+++ src/components/SidebarSetting.jsx
@@ -1,4 +1,4 @@
-import styles from '../styles/DesktopChatList.module.css';
+import styles from "../styles/SidebarSettings.module.css";
 import { ChatBubbleLeftIcon, PhoneIcon,  UserGroupIcon,  
   BuildingStorefrontIcon , ArchiveBoxIcon, MegaphoneIcon,
 PhotoIcon, CogIcon,} from "@heroicons/react/24/outline";
@@ -9,32 +9,32 @@ function SideBarSettings () {
     return (
         <div className={styles.asideSettings}>
           <div className={styles.ChatBubbleLeftIcon}>
-            <ChatBubbleLeftIcon className={styles.icon} width={50} />
+            <ChatBubbleLeftIcon className={styles.icon} />
           </div>
           <div className={styles.PhoneIcon}>
-            <PhoneIcon className={styles.icon} width={50} />
+            <PhoneIcon className={styles.icon} />
 
           </div>
           <div className={styles.StatusIcon}>
-            <HiStatusOnline className={styles.icon} size={50} />
+            <HiStatusOnline className={styles.icon} />
         </div>
         <div className={styles.UserGroupIcon}>
-          <UserGroupIcon className={styles.icon} width={50} />
+          <UserGroupIcon className={styles.icon}/>
         </div>
         <div className={styles.ArchiveBoxIcon}>
-          <ArchiveBoxIcon className={styles.icon} width={50} />
+          <ArchiveBoxIcon className={styles.icon} />
         </div>
         <div className={styles.BuildingStorefrontIcon}>
-          <BuildingStorefrontIcon className={styles.icon} width={50} />
+          <BuildingStorefrontIcon className={styles.icon}/>
         </div>
         <div className={styles.MegaphoneIcon}>
-          <MegaphoneIcon className={styles.icon} width={50} />
+          <MegaphoneIcon className={styles.icon}  />
         </div>
         <div className={styles.PhotoIcon}>
-          <PhotoIcon className={styles.icon} width={50} />
+          <PhotoIcon className={styles.icon}  />
         </div>
         <div className={styles.CogIcon}>
-          <CogIcon className={styles.icon} width={50} />
+          <CogIcon className={styles.icon} />
         </div>
         </div>
     )
